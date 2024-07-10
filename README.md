26684d5f381cbe9481f971fcfa4f64af313d54eb
JinxYTW <tyohann974@gmail.com>
2024-07-10 08:06:04 +0200

    Extensibilite du code

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# XMLToHTML
+# XML To HTML
+Voici un petit projet personnel que j'ai réalisé afin de comprendre le format XML dans un premier temps à l'aide du site W3schools.
+Par la suite, ayant entendu lors d'un stage la possibilité de créer une page HTML en partant d'un fichier XML, je m'y suis essayé avec des XML assez basiques.
+
+# Progrès
+Voici l'avancement et l'évolution du projet :
+
+## Book_Version
+
+Cette version est celle avec laquelle j'ai commencé, et qui a connu certaines optimisations, où j'avoue avoir utilisé l'intelligence artificielle pour obtenir la forme qui se trouve sur mon GitHub.
+En effet, la version précédente comportait énormément de *case*, ce qui rendait le tout peu lisible et peu extensible.
+
+## Universal_Version
+
+Ici, je suis parti de la book_version, et j'ai essayé d'en faire quelque chose de plus universelle comme l'indique le titre.
+En effet, avant j'ai essayé des nouvelles balises html, avec un peu plus de personnalisation avancée.
+Cependant, je trouvais que l'intérêt de passer d'un fichier XML à de l'HTML était simplement de créer quelque chose rapidement sans trop être permissif.
+Je suis donc parvenu à coder cette version qui fonctionne dans pas mal de cas, même si du côté du css *styleun.css*, il manque un petit quelque chose.
+
+# Prise en charge
+
+ 1. Balise simple
+ 2. Bouton (si le nom de la balise XML est button)
+ 3. Input (si le nom de la balise XML est input)
+ 4. Select (si le nom de la balise XML est select)
+ 5. Option ((si le nom de la balise XML est option)
+ 6. Gestion des Attributs
+ 7. Gestion du texte
+
+## How to use
+On *xmlparsertohtml.js*, replace book.html in line 9.
+ 
+
+```
---
+++ universal_version/js/xmlparsertohtml.js
@@ -0,0 +1,88 @@
+var xhttp = new XMLHttpRequest(); // Créer un objet XMLHttpRequest
+xhttp.onreadystatechange = function() { // Quand l'état de la requête change
+    if (this.readyState == 4 && this.status == 200) { // Si la requête est terminée et que le statut est 200
+        var xml = this.responseText; // Récupérer le contenu de la réponse
+        var html = xmlparserToHtml(xml); // Convertir le xml en html
+        document.getElementById("content").innerHTML = html; // Afficher le html dans la balise avec l'id "content"
+    }
+}
+xhttp.open("GET", "./xml/book.xml", true); // Ouvrir une requête GET pour le fichier book.xml
+xhttp.send(); // Envoyer la requête
+
+function xmlparserToHtml(xml) {
+    var html = ''; // L'attribut qui contiendra le code html
+    var parser = new DOMParser(); // L'XML parser
+    var xmlDoc = parser.parseFromString(xml, "text/xml"); // Parse le xml
+    var nodes = xmlDoc.documentElement.childNodes; // Récupère les noeuds du document,c'est à dire les noeuds enfants du noeud racine genre les <>
+    for (var i = 0; i < nodes.length; i++) {
+        html += nodeToHtml(nodes[i]);// Convertir le noeud en html
+    }
+    console.log(html);
+    return html;
+}
+
+function nodeToHtml(node) {
+    let html = ''; // L'attribut qui contiendra le code html
+
+    if (node.nodeType == 1) { // Si le noeud est un élément
+        const nodeName = node.nodeName.toLowerCase();
+
+        // Traiter les différents éléments de formulaire
+        // Si l'élément est un bouton
+        if (nodeName === 'button') {
+            const id = node.getAttribute('id');
+            const text = node.getAttribute('text');
+            const action = node.getAttribute('action');
+            html += `<button id="${id}" onclick="${action}">${text}</button>`;
+        } 
+        // Si l'élément est un menu déroulant
+        else if (nodeName === 'select') {
+            const id = node.getAttribute('id');
+            html += `<select id="${id}">`;
+            for (let i = 0; i < node.childNodes.length; i++) {
+                if (node.childNodes[i].nodeType === 1) {
+                    html += nodeToHtml(node.childNodes[i]);
+                }
+            }
+            html += `</select>`;
+        } 
+        // Si l'élément est une option
+        else if (nodeName === 'option') {
+            const value = node.getAttribute('value');
+            html += `<option value="${value}">${node.textContent}</option>`;
+        } 
+        // Si l'élément est un input
+        else if (nodeName === 'input') {
+            const id = node.getAttribute('id');
+            const type = node.getAttribute('type');
+            const placeholder = node.getAttribute('placeholder');
+            html += `<input id="${id}" type="${type}" placeholder="${placeholder}" />`;
+        } 
+        // Si l'élément est un label
+        else {
+            // Ouvrir la balise en utilisant le nom de l'élément XML et ses attributs
+            html += `<div class="${nodeName}" ${attributesToHtml(node.attributes)}>`;
+
+            for (let i = 0; i < node.childNodes.length; i++) {
+                html += nodeToHtml(node.childNodes[i]); // Convertir le noeud en html
+            }
+
+            // Fermer la balise
+            html += `</div>`;
+        }
+    } 
+    // Si le noeud est un texte
+    else if (node.nodeType == 3) { 
+        html += node.nodeValue; // Ajouter le texte directement
+    }
+
+    return html;
+}
+
+function attributesToHtml(attributes) {
+    let html = '';
+    for (let i = 0; i < attributes.length; i++) {
+        html += ` data-${attributes[i].name}="${attributes[i].value}"`;
+    }
+    return html;
+}
